5e6bc1d7f810d9cabc47a6a2f3177afdc9944351
Khazirov <khazirov804@mail.ru>
2025-01-19 21:10:37 +0300

    Add datetimepicker plugin

--- app.rb
+++ app.rb
@@ -4,73 +4,75 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+# Определяем метод get_db до его использования
+
+
 configure do
-	db = get_db
-	db.execute 'CREATE TABLE IF NOT EXISTS
-	 	 Users
-	 	(
-	  		id INTEGER PRIMARY KEY AUTOINCREMENT,
-	  		name TEXT,
-	  		phone TEXT,
-	  		datestamp TEXT,
-	  		barber TEXT,
-	  		color TEXT
-	  	)'
+  db = get_db
+  db.execute 'CREATE TABLE IF NOT EXISTS
+    Users
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      phone TEXT,
+      datestamp TEXT,
+      barber TEXT,
+      color TEXT
+    )'
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/about' do
-	erb :about
+  erb :about
 end
 
 get '/visit' do
-	erb :visit
+  erb :visit
 end
 
 get '/contacts' do
-	erb :contacts
+  erb :contacts
 end
 
 get '/' do
-	erb :index
+  erb :index
 end
 
 post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
 
-	@username = params[:username]
-	@phone = params[:phone]
-	@datetime = params[:datetime]
-	@barber = params[:barber]
-	@color = params[:color]
+  # hash
+  hh = {:username =>'Введите имя',
+        :phone => 'Введите телефон',
+        :datetime => 'Введите дату и время'}
+  @error = hh.select {|key,_| params[key] == ""}.values.join(",")
 
-	# hash
-	hh = {:username =>'Введите имя',
-			:phone => 'Введите телефон',
-			:datetime => 'Введите дату и время'}
-	@error = hh.select {|key,_| params[key] == ""}.values.join(",")
+  if @error != ''
+    return erb :visit
+  end
 
-	if @error != ''
-		return erb :visit
-	end
+  db = get_db
+  db.execute 'insert into
+  Users
+  (
+    name,	
+    phone,
+    datestamp,
+    barber,
+    color
+  )
+  values (?,?,?,?,?)',[@username,@phone,@datetime,@barber,@color]
 
-	db = get_db
-	db.execute 'insert into
-	Users
-	(
-		name,
-		phone,
-		datestamp,
-		barber,
-		color
-	)
-	values (?,?,?,?,?)',[@username,@phone,@datetime,@barber,@color]
-
-	erb "OK, Имя:#{@username}, Телефон: #{@phone}, Дата и время: #{@datetime}, Парикмахер: #{@barber}, Цвет: #{@color} " 
+  erb "OK, Имя:#{@username}, Телефон: #{@phone}, Дата и время: #{@datetime}, Парикмахер: #{@barber}, Цвет: #{@color}" 
 end
 
 def get_db
-	return SQLite3::Database.new 'barbershop.db'
+  return SQLite3::Database.new 'barbershop.db'
 end
